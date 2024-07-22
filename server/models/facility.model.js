f0cc8c4c13a5a03507f3598742c02591b8d23dc5
Odd-Doc <86434122+Odd-Doc@users.noreply.github.com>
2024-07-22 13:13:18 +0000

    facility.model added

---
+++ server/models/facility.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+//backflow device schema & model
+
+const facilitySchema = new Schema({
+    name: { type: String, required: true },
+    address: { 
+        street:{ type: String, required: true },
+        city:{ type: String, required: true },
+        state:{ type: String, required: true },
+        zip:{ type: Number, required: true },
+    },
+    devices: {type: mongoose.Schema.Types.ObjectId,
+    ref: 'Device'}
+})
+
+module.exports = mongoose.model("Device", deviceSchema);
